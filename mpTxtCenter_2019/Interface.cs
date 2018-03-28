1cce8d5c7fc1765071ce514087aafbc4820c128a
Modis <cjmodis@gmail.com>
2018-03-29 00:18:39 +0300

    autocad 2019

---
+++ mpTxtCenter_2019/Interface.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using ModPlusAPI.Interfaces;
+
+namespace mpTxtCenter
+{
+    public class Interface : IModPlusFunctionInterface
+    {
+        public SupportedProduct SupportedProduct => SupportedProduct.AutoCAD;
+        public string Name => "mpTxtCenter";
+        public string AvailProductExternalVersion => "2019";
+        public string FullClassName => string.Empty;
+        public string AppFullClassName => string.Empty;
+        public Guid AddInId => Guid.Empty;
+        public string LName => "Текст по центру";
+        public string Description => "Функция позволяет создавать однострочный текст или выравнивать существующий по середине между двумя указанными точками";
+        public string Author => "Пекшев Александр aka Modis";
+        public string Price => "0";
+        public bool CanAddToRibbon => true;
+        public string FullDescription => string.Empty;
+        public string ToolTipHelpImage => string.Empty;
+        public List<string> SubFunctionsNames => new List<string>();
+        public List<string> SubFunctionsLames => new List<string>();
+        public List<string> SubDescriptions => new List<string>();
+        public List<string> SubFullDescriptions => new List<string>();
+        public List<string> SubHelpImages => new List<string>();
+        public List<string> SubClassNames => new List<string>();
+    }
+}
